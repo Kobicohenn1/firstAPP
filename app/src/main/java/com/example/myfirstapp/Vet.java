fe0ee6b76830e8def26d71e4ce00e54763fad374
ronmoyal <ronmoyal1902@gmail.com>
2021-11-29 18:14:26 +0200

    asdf

---
+++ app/src/main/java/com/example/myfirstapp/Vet.java
@@ -0,0 +1,16 @@
+package com.example.myfirstapp;
+
+public class Vet {
+
+
+        public String fullname,email;
+
+        public Vet(String fullname,String email){
+            this.fullname=fullname;
+            this.email=email;
+
+        }
+
+    }
+
+
